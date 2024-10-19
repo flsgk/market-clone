31f7ec07cc2ca2253343508e4957a81f0ad1c6fd
flsgk <harinjung.az@gmail.com>
2024-10-19 16:27:24 +0900

    📍 svelte, vite 로 변경 후 firebase deploy 세팅

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Login from "./pages/Login.svelte";
+  import Main from "./pages/Main.svelte";
+  import NotFound from "./pages/NotFound.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import Write from "./pages/Write.svelte";
+  import Router from "svelte-spa-router";
+  import "./CSS/style.css";
+
+  const routes = {
+    "/": Main,
+    "/login": Login,
+    "/signup": Signup,
+    "/write": Write,
+    "*": NotFound,
+  };
+</script>
+
+<main>
+  <Router {routes}></Router>
+</main>
---
+++ src/pages/Signup.svelte
@@ -0,0 +1,31 @@
+<form id="signup-form" action="/signup" method="post">
+  <!-- 폼을 어느 주소로 보낼 건지를 입력하는 부분 : 주소에 /signup.html을 압력하면 확인 가능 -->
+  <div>
+    <h1>회원가입</h1>
+  </div>
+
+  <div>
+    <label for="id">아이디</label>
+    <input type="text" id="id" name="id" required />
+  </div>
+  <div>
+    <label for="password">비밀번호</label>
+    <input type="password" id="password" name="password" required />
+  </div>
+  <div>
+    <label for="password2">비밀번호 확인</label>
+    <input type="password" id="password2" name="password2" required />
+  </div>
+  <div>
+    <label for="name">이름</label>
+    <input type="text" id="name" name="name" required />
+  </div>
+  <div>
+    <label for="email">이메일</label>
+    <input type="email" id="email" name="email" required />
+  </div>
+  <div>
+    <button type="submit">회원가입 하기</button>
+  </div>
+  <div id="info"></div>
+</form>
